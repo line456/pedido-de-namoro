348fd1cc26db4ab68fddfd502e928c4261faf5cb
aline <aline.s.bezerra@aluno.senai.br>
2025-05-21 09:39:53 -0500

    site pedido de namoro

---
+++ script.js
@@ -0,0 +1,13 @@
+function sim(){
+alert("você aceitou namorar comigo! :)");
+location.href = "https://youtu.be/nh7q3iZF0VU?feature=shared";
+}
+function nao(){
+    alert("você não aceitou namorar comigo! :)");
+}   
+function desvia(btn){
+    btn.style.position ='adsolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left - geraposicao(10, 90);
+    console.log('opa, desviei...');
+}
