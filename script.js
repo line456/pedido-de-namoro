a000856b31f62c472f907054eb381be9460d0538
aline <aline.s.bezerra@aluno.senai.br>
2025-05-22 09:19:25 -0500

    pedido de namoro

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function sim(){
 alert("você aceitou namorar comigo! :)");
-location.href = "https://youtu.be/nh7q3iZF0VU?feature=shared";
+location.href ="https://youtu.be/Eli3xnjM1LE?feature=shared";
 }
-function nao(){
-    alert("você não aceitou namorar comigo! :)");
-}   
 function desvia(btn){
-    btn.style.position ='adsolute';
+    alert("você não aceitou namorar comigo! :)");
+    btn.style.position ='absolute';
     btn.style.bottom = geraPosicao(10, 90);
     btn.style.left - geraposicao(10, 90);
     console.log('opa, desviei...');
+ 
 }
